f3856146a2574df6ce659f4903ffe83ec77dd75c
GatesMa <gatesma@foxmail.com>
2019-05-23 09:41:58 +0800

    修复了登陆头像权限bug

--- DesireFU-前端/pages/register/register.js
+++ DesireFU-前端/pages/register/register.js
@@ -17,7 +17,7 @@ Page({
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
+    onLoad: function () {
         if (app.globalData.userInfo) {
             this.setData({
                 userInfo: app.globalData.userInfo,
